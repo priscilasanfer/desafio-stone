59099a67dad605daff81d76425cf00c58f619cf0
Priscila Ferreira <priscilasanfer@gmail.com>
2021-01-22 08:17:07 -0300

    desafio stone

---
+++ src/br/com/priscila/Desafio.java
@@ -0,0 +1,75 @@
+package br.com.priscila;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Desafio {
+    public static void main(String[] args) {
+        List<Item> itens = new ArrayList<>();
+        List<String> emails = new ArrayList<>();
+
+        itens.add(new Item("caneta", 1, 100));
+        itens.add(new Item("apontador", 3, 4033));
+
+        emails.add("Priscila");
+        emails.add("Malu");
+        emails.add("Meg");
+        emails.add("Rafael");
+
+        try {
+            Map<String, Integer> resultado = Desafio.calcula(itens, emails);
+            System.out.println(resultado);
+        } catch (ArithmeticException e) {
+            System.out.println("Encerrando o programa devido a erro aritmetico");
+            System.exit(1);
+        }
+    }
+
+    private static Map<String, Integer> calcula(List<Item> itens, List<String> emails) {
+        Map<String, Integer> resultado = new HashMap<>();
+
+        int quantidadeDeEmails = emails.size();
+        int valorTotalDaLista = valorTotalDaCompra(itens);
+        int valorPorPessoa = valorIndividual(quantidadeDeEmails, valorTotalDaLista);
+        int diferencaEmCentavos = valorTotalDaLista - (valorPorPessoa * quantidadeDeEmails);
+
+            for (int i = 0; i < quantidadeDeEmails; i++) {
+                resultado.put(emails.get(i), valorPorPessoa);
+            }
+
+        int contadorDeEmails = 0;
+        while (diferencaEmCentavos > 0) {
+            if (contadorDeEmails >= quantidadeDeEmails) {
+                contadorDeEmails = 0;
+            }
+            resultado.put(emails.get(contadorDeEmails), valorPorPessoa + 1);
+            diferencaEmCentavos--;
+            contadorDeEmails++;
+        }
+        return resultado;
+    }
+
+    private static int valorIndividual(int quantidadeDeEmails, int valorTotolDaComrpra) {
+        int valorIndividual = 0;
+        try {
+            valorIndividual = valorTotolDaComrpra / quantidadeDeEmails;
+        } catch (ArithmeticException e) {
+            System.out.println("Erro ao tentar dividir " + e.getMessage());
+            throw e;
+        }
+        return valorIndividual;
+    }
+
+    private static int valorTotalDaCompra(List<Item> itens) {
+        int total = 0;
+        for (Item item : itens) {
+            int quantidade = item.getQuantidade();
+            int valor = item.getValor();
+            total += quantidade * valor;
+        }
+        return total;
+    }
+}
+
---
+++ src/br/com/priscila/Item.java
@@ -0,0 +1,22 @@
+package br.com.priscila;
+
+public class Item {
+    private String nome;
+    private int quantidade;
+    private int valor;
+
+    public Item(String nome, int quantidade, int valor) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+}
